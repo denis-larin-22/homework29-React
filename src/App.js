c3a962acf3b234765b121f135e494a192cf295d9
denis-larin-22 <denislarin2017@gmail.com>
2023-03-14 21:50:26 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const toggleVisible = () => setIsHidden(!isHidden);
+
+  return(
+    <div>
+      <button onClick={toggleVisible}>Toggle</button>
+
+      {isHidden ? <Card/> : null}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import './card.css';
+
+const Card = () => {
+    const [notes, setNote] = useState([]);
+    const [update, setUpdate] = useState(0);
+    const hendleUpdate = () => setUpdate(update + 1);
+
+    useEffect(() => {
+        const request = async () => {
+            const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+            const data = await response.json();
+            setNote(data.slice(0, 20));
+        }
+
+        request();
+    }, [update])
+
+    return (
+        <div className="card">
+            <h1>Notes: {notes.length}</h1>
+            <button onClick={hendleUpdate}>Update</button>
+
+            <ul>
+                {notes.map(noteItem => <li key={noteItem.id}>{noteItem.title}</li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default Card;
